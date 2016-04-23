552530de5996311c29210019a1c3d708a80e947b
N. M. Ali Hayder <ahayder@loobylab.com>
2016-04-24 02:31:57 +0600

    Tabs and Address auto complete

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 
-angular.module('app', ['ionic', 'app.foodCtrl', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'ngCordova','ngOpenFB'])
+angular.module('app', ['ionic', 'app.foodCtrl', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'ngCordova','ngOpenFB','google.places'])
 
 .run(function($ionicPlatform,ngFB) {
    ngFB.init({appId: '577646735745723'});
