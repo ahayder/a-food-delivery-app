c1acd2bddde2e5d88d4182f581b1b3a8a8ca33e9
Soumya Rauth <shoummorauth@live.co.uk>
2016-06-20 05:37:46 +0600

    social plugin 2nd push

--- www/js/controllers/invite-friends-ctrl.js
+++ www/js/controllers/invite-friends-ctrl.js
@@ -3,49 +3,19 @@ angular.module('app.inviteFriendsCtrl', [])
 .controller('inviteFriendsCtrl', function($scope) {
 
 
-    // $scope.invitationFB = function(link) {
-    //     ngFB.login().then(
-    //         function(response) {
-
-    //             if (response.status === 'connected') {
-    //                 (alert)('Facebook Signup succeeded');
-
-    //                 $scope.shareFB(link);
-
-
-    //             } else {
-    //                 (alert)('Facebook Signup failed');
-    //             }
-    //         });
-    // }
-
-
-    // $scope.shareFB = function(link) {
-
-    //     ngFB.api({
-    //         method: 'POST',
-    //         path: '/me/feed',
-    //         params: {
-    //             message: 'Welcome to food world',
-    //             link: link
-    //         }
-    //     }).then(
-    //         function() {
-    //             (alert)('The session was shared on Facebook');
-    //         },
-    //         function() {
-    //             (alert)('An error occurred while sharing this session on Facebook');
-    //         });
-    // };
-
-
-    // $scope.invitationTwitter = function() {
-    //     $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "http://blog.nraboy.com");
-    // }
-
-    // $scope.invitationTwitter = function() {
-    //     $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "http://blog.nraboy.com");
-    // }
-
+  $scope.shareNative = function() {
+    //console.log(window.plugins);
+      if (window.plugins && window.plugins.socialsharing) {
+          window.plugins.socialsharing.share("I'll be attending the session: " + $scope.session.title + ".",
+              'PhoneGap Day 2014', null, "http://pgday.phonegap.com/us2014",
+              function() {
+                  console.log("Success")
+              },
+              function (error) {
+                  console.log("Share fail " + error)
+              });
+      }
+      else console.log("Share plugin not available");
+}
 
 })
